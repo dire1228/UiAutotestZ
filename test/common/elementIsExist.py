801d5f1d4b52e3d184f222168cb86909a2e710f3
dire1228 <dire1228@163.com>
2021-07-20 22:32:05 +0800

    增加判断元素

---
+++ test/common/elementIsExist.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from selenium.webdriver.common.by import By
+from utils.logConfig import Log
+
+class ElementAction(object):
+
+    def __init__(self):
+        self.log = Log().get_log()
+
+    """判断一个元素是否存在"""
+    def element_is_exist(self, driver, seletor, by=By.XPATH):
+        flag = True
+        try:
+            driver.find_element(by=by, value=seletor)
+            self.log.debug("元素是否存在：" + str(flag))
+            return flag
+        except Exception:
+            flag = False
+            self.log.debug("元素是否存在：" + str(flag))
+            return flag
+
+    """判断一组元素是否存在"""
+    def elements_is_exist(self, driver, seletor, by=By.XPATH):
+        flag = True
+        try:
+            driver.find_elements(by=by, value=seletor)
+            self.log.debug("元素是否存在：" + str(flag))
+            return flag
+        except Exception:
+            flag = False
+            self.log.debug("元素是否存在：" + str(flag))
+            return flag
+
+if __name__ == '__main__':
+    e = ElementAction()
+    e.element_is_exist(1, 2, 3)
+
